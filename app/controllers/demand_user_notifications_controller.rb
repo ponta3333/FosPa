030ec5220a6f7756a089e39a23281c23be70e1b5
飯野裕太 <i.i.i.you.vvv.333@gmail.com>
2020-11-22 23:15:25 +0900

    add day

--- app/controllers/demand_user_notifications_controller.rb
+++ app/controllers/demand_user_notifications_controller.rb
@@ -1,10 +1,8 @@
 class DemandUserNotificationsController < ApplicationController
   def index
-    if current_demand_user
-      @notifications = current_demand_user.demand_user_notifications.page(params[:page])
-      @notifications.where(checked: false).each do |notification|
-        notification.update_attributes(checked: true)
-      end
+    @notifications = current_demand_user.demand_user_notifications.page(params[:page]).per(10)
+    @notifications.where(checked: false).each do |notification|
+      notification.update_attributes(checked: true)
     end
   end
 end
